e3a788f364f2500c236dadf4258fd19b8bfa7897
Yannick <yannick1691@users.noreply.github.com>
2017-11-20 21:40:49 +0100

    Added warning when you don't have permission to edit, removed uneeded code

--- components/sideNav/index.js
+++ components/sideNav/index.js
@@ -7,7 +7,39 @@ import BoardLink from '../boardLink';
 import NewBoard from '../newBoard';
 
 export default class Sidenav extends Component {
+  constructor() {
+    super();
+    this.renderWarning = this.renderWarning.bind(this);
+  }
+
+  renderWarning(teamName, addBoard) {
+    return (
+      <header class={style.sidebar}>
+        <nav>
+          <Link activeClassName={style.activeBoard} href={`/team/${teamName}`} >overview</Link>
+          <div class={style.boardLinkWrapper}>
+            <h3 class={style.boardLinkHeader}>Boards</h3>
+            <ul className="mdc-list" style={`padding: 0;`}>
+              {
+                Object
+                  .keys(this.props.boards)
+                  .map(key => <BoardLink key={key} index={key} removeBoard={this.props.removeBoard} details={this.props.boards[key]} teamName={teamName} />)
+              }
+            </ul>
+            <NewBoard addBoard={this.props.addBoard} />
+          </div>
+        </nav>
+        <div class={style.warningPerm}>
+          <p>Warning, you don't have the required permissions to edit this board. <br />Any changes you make will be lost.</p>
+        </div>
+      </header>
+    );
+  }
+
   render({ teamName }, { addBoard }) {
+    if (this.props.members[`${this.props.uid}`] !== true) {
+      return <header class={style.sidebar}>{this.renderWarning(teamName, addBoard)}</header>;
+    }
     return (
       <header class={style.sidebar}>
         <nav>
