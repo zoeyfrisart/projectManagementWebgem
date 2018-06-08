528ec41fb8d3ad85fbf8c71473593dae8fc562c2
Yannick <yannick1691@users.noreply.github.com>
2018-06-08 11:16:38 +0200

    [FIX] Moves files to proper directories

---
+++ raven.js
@@ -0,0 +1,2 @@
+<script src="https://cdn.ravenjs.com/3.19.1/raven.min.js"
+  crossorigin="anonymous"></script><script>Raven.config('https://a86a9be7dd884d158fe8c14b89bdaee4@sentry.io/239006').install();</script>
---
+++ static/OneSignalSDKUpdaterWorker.js
@@ -0,0 +1,2 @@
+importScripts('https://bordderwijze.tk/sw.js');
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
