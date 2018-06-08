86b89d978543340747a40133f35e92aa8eb17404
Yannick <yannick1691@users.noreply.github.com>
2018-06-08 11:31:03 +0200

    Delete raven.js

--- raven.js
+++
@@ -1,2 +0,0 @@
-<script src="https://cdn.ravenjs.com/3.19.1/raven.min.js"
-  crossorigin="anonymous"></script><script>Raven.config('https://a86a9be7dd884d158fe8c14b89bdaee4@sentry.io/239006').install();</script>
